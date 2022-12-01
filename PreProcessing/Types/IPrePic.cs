e43ed0f5bc83fb25d9eaeac0a89dcf74dddafd1a
dev202223 <dev202223@outlook.com>
2022-12-01 06:40:12 +0330

    Keeping the necessary files

--- PreProcessing/Types/IPrePic.cs
+++
@@ -1,9 +0,0 @@
-interface IPrePic
-{
-    public string Title { get; set; }
-    public int SentencesCount { get; set; }
-    public string Category1 { get; set; }
-    public string? Category2 { get; set; }
-    public string? Category3 { get; set; }
-    public string Link { get; set; }
-}
